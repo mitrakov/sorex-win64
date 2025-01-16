93800ed6c4b5476c0c439cb91b55210bf0329836
Tommy Windows <mitrakov-artem@yandex.ru>
2025-01-16 16:59:27 +0300

    Add WPF markdown dependency

---
+++ SorexMarkdown/Data.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SorexMarkdown {
+    internal class Data: INotifyPropertyChanged {
+        private string _markdown = "";
+
+        public string Markdown {
+            get { return _markdown; }
+            set {
+                if (_markdown != value) { 
+                    _markdown = value;
+                    FirePropertyChanged();
+                }
+            }
+        }
+
+        protected void FirePropertyChanged([CallerMemberName] string propertyName = "") {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
